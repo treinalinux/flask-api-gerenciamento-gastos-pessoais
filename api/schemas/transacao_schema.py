cc5f2c3184d8a10202bf4fd1920bfee0aeedc651
Alan da Silva Alves <linuxfama@gmail.com>
2021-09-25 14:05:14 -0300

    feat: transacao implemented

---
+++ api/schemas/transacao_schema.py
@@ -0,0 +1,15 @@
+from api import ma
+from ..models import transacao_model
+from marshmallow import fields
+
+
+class TransacaoSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = transacao_model.Transacao
+        load_instance = True
+
+    nome = fields.String(required=True)
+    descricao = fields.String(required=True)
+    valor = fields.Float(required=True)
+    tipo = fields.String(required=True)
+    conta_id = fields.Integer(required=True)
